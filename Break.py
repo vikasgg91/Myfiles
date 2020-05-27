7297a86fac2d740a1f48975766d99f528bbaea9f
vikasgg91 <65938604+vikasgg91@users.noreply.github.com>
2020-05-27 13:10:06 +0530

    This is program on how to use while loop

---
+++ Break.py
@@ -0,0 +1,9 @@
+while True:
+    s=raw_input('Enter something: ')
+    if s=="Quit":
+        print 'Length sentence: ',len(s)
+        break
+        print hello
+    else:
+        print 'Length of the sentence: ',len(s)
+print "bye"
